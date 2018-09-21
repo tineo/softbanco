86a7eff94aaa6f422686060d92cb7ab214e2b027
Cesar Gutierrez <cesar@tineo.mobi>
2018-09-21 13:14:22 -0500

    fisrt commit

---
+++ src/main/java/com/softbanco/repositories/RecordRepository.java
@@ -0,0 +1,18 @@
+package com.softbanco.repositories;
+
+import com.softbanco.entities.Record;
+import java.util.List;
+import javax.ejb.Remote;
+
+/**
+ * Record repository abstract interface.
+ * 
+ * @author Petr Smrcek <smrkous@gmail.com>
+ */
+@Remote
+public interface RecordRepository {
+    public void create(Record r);
+    public void delete(int id);
+    public void updateContent(int id, String content);
+    public List<Record> getAll();
+}
---
+++ src/main/java/com/softbanco/servlets/OperacionesServlet.java
@@ -0,0 +1,26 @@
+package com.softbanco.servlets;
+
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+
+@WebServlet(name = "OperacionesServlet", urlPatterns = {"/json"})
+public class OperacionesServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String ejm = "oe/e";
+        String json = new Gson().toJson(ejm);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.getWriter().write(json);
+    }
+}
